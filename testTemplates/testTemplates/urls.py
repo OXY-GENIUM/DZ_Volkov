c8227bd173b1d7340e4afe0c1b0d414cfe54eb7f
OXY_GENIUM <daniil-nuzhin@yandex.ru>
2025-01-11 16:44:47 +0300

    magaz project changed

--- testTemplates/testTemplates/urls.py
+++ testTemplates/testTemplates/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import *
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', main_page),
+    path('catalog', catalog_page),
+    path('about', about_page),
 ]

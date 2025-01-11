1044908a1ec47acee51a6c6f73a7485d18acbf2c
OXY_GENIUM <daniil-nuzhin@yandex.ru>
2025-01-11 23:48:28 +0300

    2/2 blog project changed

--- testTemplates/testTemplates/views.py
+++ testTemplates/testTemplates/views.py
@@ -4,16 +4,16 @@ from django.shortcuts import render
 MENU = {"Главная": "/", "О блоге": "/catalog", "Страница поста": "/about"}
 
 def main_page(request):
-    title = "Главная страница"
+    title = "AI Blog - что это?"
     data = {"menu": MENU, "title": title}  # Передаём саму переменную MENU
     return render(request, "index.html", context=data)
 
 def catalog_page(request):
-    title = "Каталог"
+    title = "О блоге"
     data = {"menu": MENU, "title": title}
     return render(request, "catalog.html", context=data)
 
 def about_page(request):
-    title = "О компании"
+    title = "Как сделать игру более привлекательной с помощью VR & AI"
     data = {"menu": MENU, "title": title}
     return render(request, "about.html", context=data)
